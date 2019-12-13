0e6d7dd6432f09bdf0f2b6d6ec587f3259ad2b21
Youssef Magdi <yoouyoou@gmail.com>
2019-12-13 22:41:46 +0000

    Added navbar and contact page

--- ym00335_com2025_coursework/config/routes.rb
+++ ym00335_com2025_coursework/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'home#home'
+  get 'contact', to: 'home#contact'
+
+  resources :posts
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+
 end
